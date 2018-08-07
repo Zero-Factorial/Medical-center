cab70054bae2d63f34e1fd059fdeb1399f3376ef
Zero-Factorial <rifat.cseru@gmail.com>
2018-08-07 21:18:36 +0600

    ERROR is handled on index file

--- index.js
+++ index.js
@@ -19,9 +19,30 @@ app.use('/medical', require('./routes/staffs'));
 //app.use('/medical', require('./routes/tests'));
 //app.use('/medical', require('./routes/schedules'));
 
+
 app.use(function(err,req,res,next){
    res.status(422).send({error:err.message});
 });
+
+
+app.use((req, res, next)=>{
+
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+
+});
+
+app.use((error, req, res, next) => {
+
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
+
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
